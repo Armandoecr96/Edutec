b5a5aae49300f9c7a3e948bdff134723faea72ec
Armando Carvajal <armandoecr_96@hotmail.com>
2019-06-06 11:07:24 -0500

    fix issue on play gif

--- android/app/src/main/java/com/edutec/MainApplication.java
+++ android/app/src/main/java/com/edutec/MainApplication.java
@@ -3,6 +3,7 @@ package com.edutec;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.moussawi7.controlledgif.ControlledGifPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ControlledGifPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
             new RNSoundPackage(),
